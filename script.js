2d52ed44dd4371e9ba6f16e81cb017ec0aed9ef2
Thenula Panapiti <120708576+darkhackersl@users.noreply.github.com>
2024-11-03 13:15:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Thank you for your message!');
     this.reset();
+
+});
+
+document.querySelectorAll('.product-item button').forEach(button => {
+    button.addEventListener('click', function() {
+        cartCount++;
+        document.getElementById('cart-count').innerText = cartCount;
+        alert(`${this.parentElement.querySelector('h3').innerText} added to cart!`);
+    });
 });
